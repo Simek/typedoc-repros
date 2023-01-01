12be40f6fb8259a1c108219d62fbb22998ef270d
Simek <gosimek@gmail.com>
2023-01-01 19:13:40 +0100

    simplify repro

--- src/Camera.types.ts
+++
@@ -1,6 +0,0 @@
-import { PermissionResponse } from 'expo-modules-core';
-import { ViewProps } from 'react-native';
-
-export type CameraProps = ViewProps;
-
-export { PermissionResponse };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-export { default as Camera } from './Camera';
-export {
-  getCameraPermissionsAsync,
-  requestCameraPermissionsAsync,
-} from './Camera';
+import { createPermissionHook, PermissionResponse } from 'expo-modules-core';
+
+export default class Camera {
+  // note(simek): comment in there is overwritten by default `createPermissionHook` description
+  /**
+   * Check or request permissions to access the camera.
+   * This uses both `requestCameraPermissionsAsync` and `getCameraPermissionsAsync` to interact with the permissions.
+   * @example
+   * ```ts
+   * const [status, requestPermission] = Camera.useCameraPermissions();
+   * ```
+   */
+  static useCameraPermissions = createPermissionHook({
+    getMethod: async () => ({} as PermissionResponse),
+    requestMethod: async () => ({} as PermissionResponse),
+  });
+}
